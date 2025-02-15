4e922d382a13d9332512cd8d9517154c19a5a8dc
srdtrk <srdtrk@hotmail.com>
2025-02-15 14:31:39 -0500

    docs: admin stuff

--- src/cli/mod.rs
+++ src/cli/mod.rs
@@ -1,1 +1,3 @@
+//! This module contains the command line interface and the config for the application.
+
 pub mod cmd;
--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,9 @@
-pub mod cli;
+//! # Natlint
+//! A linter for solidity natspecs.
 
+// TODO: Use README.md as the crate documentation when we have a readme
+//#![doc = include_str!("../README.md")]
+#![deny(clippy::nursery, clippy::pedantic, warnings, missing_docs)]
+
+pub mod cli;
+pub mod parser;
