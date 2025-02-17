8a6c77c221db913b79e03f4fa5541ef04d217073
srdtrk <srdtrk@hotmail.com>
2025-02-17 23:18:42 +0300

    feat: test passing

--- src/parser/errors.rs
+++ src/parser/errors.rs
@@ -1,17 +1,14 @@
 //! Contains the error types used by the parser.
 
-#[derive(thiserror::Error, Debug)]
+#[derive(thiserror::Error, Debug, PartialEq, Eq)]
+#[allow(missing_docs)]
 pub enum NatspecParseError {
     #[error("Missing natspec tag")]
     MissingNatspecTag,
     #[error("Missing natspec description")]
     MissingNatspecDesc,
-    #[error("Missing parameter name")]
-    MissingParameterName,
     #[error("Missing parameter description")]
     MissingParameterDesc,
-    #[error("Missing return variable name")]
-    MissingReturnVarName,
     #[error("Missing return variable description")]
     MissingReturnVarDesc,
     #[error("Missing custom tag")]
