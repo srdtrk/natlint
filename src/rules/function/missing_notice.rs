59dfb0b3ddde65845fad52ca7044a38c2f20656a
srdtrk <59252793+srdtrk@users.noreply.github.com>
2025-02-28 14:08:33 +0700

    feat: implemented all function rules (#6)

---
+++ src/rules/function/missing_notice.rs
@@ -0,0 +1,184 @@
+use solang_parser::pt::FunctionDefinition;
+
+use crate::parser::{CommentTag, CommentsRef, ParseItem};
+
+use super::super::{Rule, Violation};
+
+/// This rule requires that all functions have a notice or an inheritdoc comment.
+pub struct MissingNotice;
+
+impl Rule<FunctionDefinition> for MissingNotice {
+    const NAME: &'static str = "Missing Notice";
+    const DESCRIPTION: &'static str =
+        "This rule requires that all functions have a notice or an inheritdoc comment.";
+
+    fn check(
+        _: Option<&ParseItem>,
+        func: &FunctionDefinition,
+        comments: CommentsRef,
+    ) -> Option<Violation> {
+        // If the function has an inheritdoc comment, it is exempt from this rule
+        if comments.find_inheritdoc_base().is_some() {
+            return None;
+        }
+
+        // Function must have a notice comment
+        match comments.include_tag(CommentTag::Notice).len() {
+            0 => Some(Violation::new(
+                Self::NAME,
+                "Missing notice or inheritdoc comment".to_string(),
+                func.loc,
+            )),
+            1 => None,
+            _ => Some(Violation::new(
+                Self::NAME,
+                "Too many notice comments".to_string(),
+                func.loc,
+            )),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{FunctionDefinition, MissingNotice, Rule};
+    use crate::{
+        parser::{CommentsRef, Parser},
+        rules::Violation,
+    };
+    use forge_fmt::Visitable;
+    use solang_parser::parse;
+
+    fn parse_source(src: &str) -> Parser {
+        let (mut source, comments) = parse(src, 0).expect("failed to parse source");
+        let mut doc = Parser::new(comments, src.to_owned());
+        source.visit(&mut doc).expect("failed to visit source");
+        doc
+    }
+
+    /// Macro to define a test case for `MissingParams` rule
+    macro_rules! test_require_missingparams {
+        ($name:ident, $source:expr, $expected:expr) => {
+            #[test]
+            fn $name() {
+                let src = parse_source($source);
+
+                let parent = src.items_ref().first().unwrap();
+                let child = parent.children.first().unwrap();
+                let func = child.as_function().unwrap();
+                let comments = CommentsRef::from(&child.comments);
+
+                let expected = $expected(func);
+
+                assert_eq!(MissingNotice::check(Some(parent), func, comments), expected);
+            }
+        };
+    }
+
+    test_require_missingparams!(
+        public_no_violation,
+        r"
+        contract Test {
+            /// @notice Some function
+            function test(uint256 a) public {}
+        }
+        ",
+        |_| None
+    );
+
+    test_require_missingparams!(
+        private_no_violation,
+        r"
+        contract Test {
+            /// @notice Some function
+            function test(uint256 a) private {}
+        }
+        ",
+        |_| None
+    );
+
+    test_require_missingparams!(
+        multiline_no_violation,
+        r"
+        contract Test {
+            /**
+             * @notice Some function
+             */
+            function test(uint256 a) private {}
+        }
+        ",
+        |_| None
+    );
+
+    test_require_missingparams!(
+        inheritdoc_no_violation,
+        r"
+        contract Test {
+            /// @inheritdoc something
+            function test(uint256 a) public {}
+        }
+        ",
+        |_| None
+    );
+
+    test_require_missingparams!(
+        multiline_inheritdoc_no_violation,
+        r"
+        contract Test {
+            /**
+             * @inheritdoc something
+             */
+            function test(uint256 a) public {}
+        }
+        ",
+        |_| None
+    );
+
+    test_require_missingparams!(
+        public_violation,
+        r"
+        contract Test {
+            function test(uint256 a) public {}
+        }
+        ",
+        |func: &FunctionDefinition| Some(Violation::new(
+            MissingNotice::NAME,
+            "Missing notice or inheritdoc comment".to_string(),
+            func.loc
+        ))
+    );
+
+    test_require_missingparams!(
+        too_many_comments_violation,
+        r"
+        contract Test {
+            /// @notice Some function
+            /// @notice Another function
+            function test(uint256 a) public {}
+        }
+        ",
+        |func: &FunctionDefinition| Some(Violation::new(
+            MissingNotice::NAME,
+            "Too many notice comments".to_string(),
+            func.loc
+        ))
+    );
+
+    test_require_missingparams!(
+        multiline_many_comments_violation,
+        r"
+        contract Test {
+            /**
+             * @notice Some function
+             * @notice Another function
+             */
+            function test(uint256 a) public {}
+        }
+        ",
+        |func: &FunctionDefinition| Some(Violation::new(
+            MissingNotice::NAME,
+            "Too many notice comments".to_string(),
+            func.loc
+        ))
+    );
+}
--- src/rules/function/mod.rs
+++ src/rules/function/mod.rs
@@ -1,8 +1,14 @@
 //! This module defines the rules for function items in the natlint linter.
 
+mod missing_notice;
 mod missing_params;
+mod missing_return;
+mod only_inheritdoc;
 mod require_inheritdoc;
 
 // The rules are exported here:
+pub use missing_notice::MissingNotice;
 pub use missing_params::MissingParams;
+pub use missing_return::MissingReturn;
+pub use only_inheritdoc::OnlyInheritdoc;
 pub use require_inheritdoc::RequireInheritdoc;
