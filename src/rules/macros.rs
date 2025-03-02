db3e7211a47c8b05d5060f07890ed628d896e100
srdtrk <srdtrk@hotmail.com>
2025-03-02 13:07:52 +0300

    refactor: one rule

--- src/rules/macros.rs
+++ src/rules/macros.rs
@@ -33,3 +33,37 @@ macro_rules! no_comment_rule {
         }
     };
 }
+
+/// This macro defines a rule that enforces the presence of a specific comment tag.
+#[macro_export]
+macro_rules! missing_comment_rule {
+    ($rule_name:ident, $parse_item:ty, $comment_variant:ident, $description:expr) => {
+        #[doc = $description]
+        pub struct $rule_name;
+
+        impl $crate::rules::Rule<$parse_item> for $rule_name {
+            const NAME: &'static str = stringify!($rule_name);
+            const DESCRIPTION: &'static str = $description;
+
+            fn check(
+                _: Option<&$crate::parser::ParseItem>,
+                item: &$parse_item,
+                comments: $crate::parser::CommentsRef,
+            ) -> Option<$crate::rules::Violation> {
+                if comments
+                    .include_tag($crate::parser::CommentTag::$comment_variant)
+                    .is_empty()
+                {
+                    return Some($crate::rules::Violation::new(
+                        Self::NAME,
+                        $crate::rules::violation_error::ViolationError::MissingComment(
+                            $crate::parser::CommentTag::$comment_variant,
+                        ),
+                        item.loc,
+                    ));
+                }
+                None
+            }
+        }
+    };
+}
