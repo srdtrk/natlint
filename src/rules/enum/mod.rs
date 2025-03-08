02f77ccba8e49aeb58a1953fcf5cba40803ed9f8
srdtrk <srdtrk@hotmail.com>
2025-03-08 17:44:49 +0300

    feat: added no_return

--- src/rules/enum/mod.rs
+++ src/rules/enum/mod.rs
@@ -4,9 +4,11 @@ mod missing_author;
 mod missing_notice;
 mod missing_title;
 mod no_inheritdoc;
+mod no_return;
 
 // The rules are exported here:
 pub use missing_author::MissingAuthor;
 pub use missing_notice::MissingNotice;
 pub use missing_title::MissingTitle;
 pub use no_inheritdoc::NoInheritdoc;
+pub use no_return::NoReturn;
---
+++ src/rules/enum/no_return.rs
@@ -0,0 +1,124 @@
+use solang_parser::pt::EnumDefinition;
+
+crate::no_comment_rule!(
+    NoReturn,
+    EnumDefinition,
+    return,
+    "Enums must not have a return comment."
+);
+
+#[cfg(test)]
+mod tests {
+    use super::{EnumDefinition, NoReturn};
+    use crate::{
+        parser::{CommentTag, CommentsRef, Parser},
+        rules::{violation_error::ViolationError, Rule, Violation},
+    };
+    use forge_fmt::Visitable;
+    use solang_parser::parse;
+
+    fn parse_source(src: &str) -> Parser {
+        let (mut source, comments) = parse(src, 0).expect("failed to parse source");
+        let mut doc = Parser::new(comments, src.to_owned());
+        source.visit(&mut doc).expect("failed to visit source");
+        doc
+    }
+
+    macro_rules! test_no_return {
+        ($name:ident, $source:expr, $expected:expr) => {
+            #[test]
+            fn $name() {
+                let src = parse_source($source);
+
+                let parent = src.items_ref().first().unwrap();
+                let child = parent.children.first().unwrap();
+                let item = child.as_enum().unwrap();
+                let comments = CommentsRef::from(&child.comments);
+
+                let expected = $expected(item);
+
+                assert_eq!(NoReturn::check(Some(parent), item, comments), expected);
+            }
+        };
+    }
+
+    test_no_return!(
+        empty_no_violation,
+        r"
+        interface Test {
+            enum Option {
+                Some,
+                None
+            }
+        }
+        ",
+        |_| None
+    );
+
+    test_no_return!(
+        no_violation,
+        r"
+        interface Test {
+            /// @custom:test Some comment
+            enum Option {
+                Some,
+                None
+            }
+        }
+        ",
+        |_| None
+    );
+    test_no_return!(
+        multiline_no_violation,
+        r"
+        interface Test {
+            /**
+             * @custom:test Some comment
+             */
+            enum Option {
+                Some,
+                None
+            }
+        }
+        ",
+        |_| None
+    );
+
+    test_no_return!(
+        return_violation,
+        r"
+        interface Test {
+            /// @return something
+            enum Option {
+                Some,
+                None
+            }
+        }
+        ",
+        |item: &EnumDefinition| Some(Violation::new(
+            NoReturn::NAME,
+            ViolationError::CommentNotAllowed(CommentTag::Return),
+            item.loc
+        ))
+    );
+
+    test_no_return!(
+        multiline_return_violation,
+        r"
+        interface Test {
+            /**
+             * @return something
+             */
+            enum Option {
+                Some,
+                None
+            }
+        }
+        ",
+        |item: &EnumDefinition| Some(Violation::new(
+            NoReturn::NAME,
+            ViolationError::CommentNotAllowed(CommentTag::Return),
+            item.loc
+        ))
+    );
+}
