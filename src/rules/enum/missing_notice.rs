22cbf55a5e9a05cd937f4485b26bbf20bf046bd2
srdtrk <srdtrk@hotmail.com>
2025-03-08 17:19:57 +0300

    feat: added missing notice for enums

---
+++ src/rules/enum/missing_notice.rs
@@ -0,0 +1,180 @@
+//! This rule requires that all enums have a notice comment.
+
+use solang_parser::pt::EnumDefinition;
+
+crate::missing_comment_rule!(
+    MissingNotice,
+    EnumDefinition,
+    Notice,
+    "Enums must have a notice comment."
+);
+
+#[cfg(test)]
+mod tests {
+    use super::{EnumDefinition, MissingNotice};
+    use crate::{
+        parser::{CommentTag, CommentsRef, Parser},
+        rules::{violation_error::ViolationError, Rule, Violation},
+    };
+    use forge_fmt::Visitable;
+    use solang_parser::parse;
+
+    fn parse_source(src: &str) -> Parser {
+        let (mut source, comments) = parse(src, 0).expect("failed to parse source");
+        let mut doc = Parser::new(comments, src.to_owned());
+        source.visit(&mut doc).expect("failed to visit source");
+        doc
+    }
+
+    macro_rules! test_missingnotice {
+        ($name:ident, $source:expr, $expected:expr) => {
+            #[test]
+            fn $name() {
+                let src = parse_source($source);
+
+                let parent = src.items_ref().first().unwrap();
+                let child = parent.children.first().unwrap();
+                let item = child.as_enum().unwrap();
+
+                let comments = CommentsRef::from(&child.comments);
+
+                let expected = $expected(item);
+
+                assert_eq!(MissingNotice::check(Some(parent), item, comments), expected);
+            }
+        };
+    }
+
+    test_missingnotice!(
+        no_violation,
+        r"
+        interface Test {
+            /// @notice Some notice
+            enum Option {
+                Some,
+                None
+            }
+        }
+        ",
+        |_| None
+    );
+
+    test_missingnotice!(
+        multi_no_violation,
+        r"
+        interface Test {
+            /// @notice Some notice
+            /// @custom:test Some comment
+            enum Option {
+                Some,
+                None
+            }
+        }
+        ",
+        |_| None
+    );
+
+    test_missingnotice!(
+        multi_notice_no_violation,
+        r"
+        interface Test {
+            /// @notice Some notice
+            /// @notice Some other
+            enum Option {
+                Some,
+                None
+            }
+        }
+        ",
+        |_| None
+    );
+
+    test_missingnotice!(
+        multiline_multi_no_violation,
+        r"
+        interface Test {
+            /**
+             * @notice Some notice
+             * @custom:test Some comment
+             */
+            enum Option {
+                Some,
+                None
+            }
+        }
+        ",
+        |_| None
+    );
+
+    test_missingnotice!(
+        multiline_multi_notice_no_violation,
+        r"
+        interface Test {
+            /**
+             * @notice Some notice
+             * @notice Some other
+             */
+            enum Option {
+                Some,
+                None
+            }
+        }
+        ",
+        |_| None
+    );
+
+    test_missingnotice!(
+        empty_violation,
+        r"
+        contract Test {
+            enum Option {
+                Some,
+                None
+            }
+        }
+        ",
+        |sct: &EnumDefinition| Some(Violation::new(
+            MissingNotice::NAME,
+            ViolationError::MissingComment(CommentTag::Notice),
+            sct.loc
+        ))
+    );
+
+    test_missingnotice!(
+        violation,
+        r"
+        contract Test {
+            /// @custom:test Some comment
+            enum Option {
+                Some,
+                None
+            }
+        }
+        ",
+        |sct: &EnumDefinition| Some(Violation::new(
+            MissingNotice::NAME,
+            ViolationError::MissingComment(CommentTag::Notice),
+            sct.loc
+        ))
+    );
+
+    test_missingnotice!(
+        multiline_violation,
+        r"
+        contract Test {
+            /**
+             * @custom:test Some comment
+             */
+            enum Option {
+                Some,
+                None
+            }
+        }
+        ",
+        |sct: &EnumDefinition| Some(Violation::new(
+            MissingNotice::NAME,
+            ViolationError::MissingComment(CommentTag::Notice),
+            sct.loc
+        ))
+    );
+}
--- src/rules/enum/mod.rs
+++ src/rules/enum/mod.rs
@@ -1,8 +1,10 @@
 //! This module defines the rules for enum items in the natlint linter.
 
 mod missing_author;
+mod missing_notice;
 mod missing_title;
 
 // The rules are exported here:
 pub use missing_author::MissingAuthor;
+pub use missing_notice::MissingNotice;
 pub use missing_title::MissingTitle;
