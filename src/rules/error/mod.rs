cf46da3ab395e70badb18ed9a8ea1b89198d2d63
srdtrk <srdtrk@hotmail.com>
2025-03-16 22:35:27 +0400

    feat: added missing param

--- src/rules/error/mod.rs
+++ src/rules/error/mod.rs
@@ -1,6 +1,7 @@
 //! This module defines the rules for error items in the natlint linter.
 
 mod missing_notice;
+mod missing_param;
 mod no_author;
 mod no_inheritdoc;
 mod no_return;
@@ -8,6 +9,7 @@ mod no_title;
 
 // The rules are exported here:
 pub use missing_notice::MissingNotice;
+pub use missing_param::MissingParam;
 pub use no_author::NoAuthor;
 pub use no_inheritdoc::NoInheritdoc;
 pub use no_return::NoReturn;
