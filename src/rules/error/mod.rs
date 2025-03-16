5310839fff058eafd0dc0775eed162d3c6f6249d
srdtrk <srdtrk@hotmail.com>
2025-03-16 22:17:23 +0400

    feat: added no rules to error

--- src/rules/error/mod.rs
+++ src/rules/error/mod.rs
@@ -1,3 +1,12 @@
 //! This module defines the rules for error items in the natlint linter.
 
-//use solang_parser::pt::ErrorDefinition;
+mod no_author;
+mod no_inheritdoc;
+mod no_return;
+mod no_title;
+
+// The rules are exported here:
+pub use no_author::NoAuthor;
+pub use no_inheritdoc::NoInheritdoc;
+pub use no_return::NoReturn;
+pub use no_title::NoTitle;
