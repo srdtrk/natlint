85338968ecd84b779cad340eca84d55a64829008
Gjermund Garaba <gjermund@garaba.net>
2025-03-29 21:52:46 +0300

    contract test starting point

--- tests/lib.rs
+++ tests/lib.rs
@@ -41,8 +41,40 @@ mod natspec_test {
         assert_eq!(violations.len(), 5);
 
         assert!(violations[0].0.rule_name == "MissingAuthor");
-        assert!(violations[0].1 == 6);
+        assert!(violations[0].1 == 4);
         assert!(violations[1].0.rule_name == "MissingNotice");
-        assert!(violations[1].1 == 6);
+        assert!(violations[1].1 == 4);
+        assert!(violations[2].0.rule_name == "MissingTitle");
+        assert!(violations[2].1 == 4);
+        assert!(violations[3].0.rule_name == "MissingNotice");
+        assert!(violations[3].1 == 5);
+        assert!(violations[4].0.rule_name == "MissingParams");
+        assert!(violations[4].1 == 5);
+    }
+
+    #[test]
+    fn test_msgs() {
+        let file_path = Path::new("tests/contracts/TestMsgs.sol");
+        let content = fs::read_to_string(file_path).expect("Failed to read test file");
+
+        let config = load_default_config();
+        let violations: Vec<_> = lint(&content, &config).expect("Failed to process file");
+
+        assert_eq!(violations.len(), 7);
+
+        assert!(violations[0].0.rule_name == "MissingAuthor");
+        assert!(violations[0].1 == 4);
+        assert!(violations[1].0.rule_name == "MissingNotice");
+        assert!(violations[1].1 == 4);
+        assert!(violations[2].0.rule_name == "MissingTitle");
+        assert!(violations[2].1 == 4);
+        assert!(violations[3].0.rule_name == "MissingAuthor");
+        assert!(violations[3].1 == 5);
+        assert!(violations[4].0.rule_name == "MissingNotice");
+        assert!(violations[4].1 == 5);
+        assert!(violations[5].0.rule_name == "MissingParams");
+        assert!(violations[5].1 == 5);
+        assert!(violations[6].0.rule_name == "MissingTitle");
+        assert!(violations[6].1 == 5);
     }
 }
