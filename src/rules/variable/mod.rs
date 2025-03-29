60283746d27ec41680f96964c599737f7ccbd183
srdtrk <srdtrk@hotmail.com>
2025-03-29 13:25:59 +0300

    imp: new rule

--- src/rules/variable/mod.rs
+++ src/rules/variable/mod.rs
@@ -5,6 +5,7 @@ mod no_author;
 mod no_param;
 mod no_return;
 mod no_title;
+mod too_many_notice;
 
 // The rules are exported here:
 pub use missing_notice::MissingNotice;
@@ -12,3 +13,4 @@ pub use no_author::NoAuthor;
 pub use no_param::NoParam;
 pub use no_return::NoReturn;
 pub use no_title::NoTitle;
+pub use too_many_notice::TooManyNotice;
