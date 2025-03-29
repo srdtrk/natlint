ec96b34aef837de9926c6942cc95dbbc20fc8889
srdtrk <srdtrk@hotmail.com>
2025-03-29 13:11:09 +0300

    feat: new rule

--- src/rules/variable/mod.rs
+++ src/rules/variable/mod.rs
@@ -2,7 +2,9 @@
 
 mod missing_notice;
 mod no_author;
+mod no_title;
 
 // The rules are exported here:
 pub use missing_notice::MissingNotice;
 pub use no_author::NoAuthor;
+pub use no_title::NoTitle;
