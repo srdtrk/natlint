7060907b637a6a61f1c206a4ef619c1d8c5f7f92
srdtrk <srdtrk@hotmail.com>
2025-05-18 01:24:42 +0400

    imp: took bin impl

--- src/bin/natlint.rs
+++ src/bin/natlint.rs
@@ -1,18 +1,63 @@
+use std::fs;
+
 use clap::Parser;
+use itertools::Itertools;
 use natlint::{
-    cli::cmd::{Commands, NatlintCli},
-    cli::file_finder::find_matching_files,
+    cli::{
+        cmd::{Commands, NatlintCli},
+        file_finder::find_matching_files,
+    },
+    config::Config,
+    linter::lint,
+    rules::Violation,
 };
 
 fn main() -> eyre::Result<()> {
     let cli = NatlintCli::parse();
     match cli.command {
         Commands::Run(args) => {
-            println!("Running natlint with config: {}", args.config);
+            // TODO: Load specified config, default config path or default config
+            let config = Config::default();
 
-            // Find all files in the root directory that match the include globs and do not match the exclude globs
-            let files = find_matching_files(&args.root, args.include, args.exclude)?;
-            files.iter().for_each(|file| println!("{}", file.display()));
+            let file_violations: Vec<(String, Vec<(Violation, usize)>)> =
+                find_matching_files(&args.root, args.include, args.exclude)?
+                    .iter()
+                    .map(|file| {
+                        let content = fs::read_to_string(file).unwrap();
+                        let file_path = file.to_str().unwrap().to_owned();
+
+                        (file_path, lint(&content, &config.rules()).unwrap())
+                    })
+                    .sorted_by(|(file_a, _), (file_b, _)| file_a.cmp(file_b))
+                    .collect::<Vec<_>>();
+
+            let mut file_count = 0;
+            let mut violation_count = 0;
+
+            file_violations
+                .iter()
+                .for_each(|(file, violation_with_line)| {
+                    file_count += 1;
+
+                    println!("\nFile: {}", file);
+                    for (violation, line_number) in violation_with_line {
+                        // Print violation details with converted line number
+                        println!(
+                            "  [{}] Line {}: {}",
+                            violation.rule_name, line_number, violation.rule_description
+                        );
+                        violation_count += 1;
+                    }
+                });
+
+            if violation_count != 0 {
+                println!(
+                    "\nFound {} natspec violations in {} files.",
+                    violation_count, file_count
+                );
+                // Return non-zero exit code if violations were found
+                std::process::exit(1);
+            }
 
             Ok(())
         }
