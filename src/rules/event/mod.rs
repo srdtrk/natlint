993bc80f288f5af6cdc76dad53f26b023bfafb84
srdtrk <srdtrk@hotmail.com>
2025-05-18 13:02:18 +0400

    imp: added no inheritdoc

--- src/rules/event/mod.rs
+++ src/rules/event/mod.rs
@@ -3,8 +3,10 @@
 mod missing_notice;
 mod missing_param;
 mod no_author;
+mod no_inheritdoc;
 
 // The rules are exported here:
 pub use missing_notice::MissingNotice;
 pub use missing_param::MissingParam;
 pub use no_author::NoAuthor;
+pub use no_inheritdoc::NoInheritdoc;
