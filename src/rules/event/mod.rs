d35f179f99af7aeb3970c049d52023f2b2d61baf
srdtrk <srdtrk@hotmail.com>
2025-05-18 13:07:41 +0400

    imp: added no title

--- src/rules/event/mod.rs
+++ src/rules/event/mod.rs
@@ -5,6 +5,7 @@ mod missing_param;
 mod no_author;
 mod no_inheritdoc;
 mod no_return;
+mod no_title;
 
 // The rules are exported here:
 pub use missing_notice::MissingNotice;
@@ -12,3 +13,4 @@ pub use missing_param::MissingParam;
 pub use no_author::NoAuthor;
 pub use no_inheritdoc::NoInheritdoc;
 pub use no_return::NoReturn;
+pub use no_title::NoTitle;
