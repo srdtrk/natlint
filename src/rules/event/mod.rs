5c0cabf7a1ad65e6c9dd32a3ea80378be80a70ce
srdtrk <srdtrk@hotmail.com>
2025-05-18 12:59:56 +0400

    imp: added no author

--- src/rules/event/mod.rs
+++ src/rules/event/mod.rs
@@ -2,7 +2,9 @@
 
 mod missing_notice;
 mod missing_param;
+mod no_author;
 
 // The rules are exported here:
 pub use missing_notice::MissingNotice;
 pub use missing_param::MissingParam;
+pub use no_author::NoAuthor;
