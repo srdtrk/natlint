593062ff5f2e74c520c59527794fd90f47c70cf0
srdtrk <srdtrk@hotmail.com>
2025-05-25 12:06:54 +0400

    imp: include README in rustdoc

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,4 @@
-//! # Natlint
-//! A linter for solidity natspecs.
-
-// TODO: Use README.md as the crate documentation when we have a readme
-//#![doc = include_str!("../README.md")]
+#![doc = include_str!("../README.md")]
 #![deny(clippy::nursery, clippy::pedantic, warnings, missing_docs)]
 
 pub mod cli;
